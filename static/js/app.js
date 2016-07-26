c353466ea202d913ad2db5195d2509cc562b4763
ZodiacFireworks <martin.vuelta@gmail.com>
2016-07-26 01:20:36 -0500

    Femicide Map - Perú
    Change log:
      * Fast testing with afghan map
    
    Modified files:
      * modified:   index.html
      * modified:   static/css/app.css
      * new file:   static/data/af-all.geo.json
      * new file:   static/data/gh-all.geo.json
      * new file:   static/data/pe-all.geo.json
      * modified:   static/js/app.js
      * new file:   static/js/datamaps.world.hires.js
      * new file:   static/js/datamaps.world.js

---
+++ static/js/app.js
@@ -0,0 +1,33 @@
+(function () {
+    $(window).load(function () {
+        $(document).ready(function () {
+            var map = new Datamap({
+                element: document.getElementById('map-canvas'),
+                responsive: true,
+                geographyConfig: {
+                  dataUrl: './static/data/af-all.geo.json'
+                },
+                scope: 'afghan',
+                fills: {
+                  defaultFill: '#bada55',
+                  someKey: '#fa0fa0'
+                },
+                data: {
+                  'AFG-1758': {fillKey: 'someKey'},
+                  'AFG-1747': {fillKey: 'someKey'}
+                },
+                setProjection: function(element) {
+                  var projection = d3.geo.mercator()
+                    .center([66.166667, 34.4444])
+                    .scale(2200)
+                    .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+
+                   var path = d3.geo.path().projection(projection);
+                   return {path: path, projection: projection};
+                }
+            });
+
+            window.addEventListener('resize', function() { map.resize(); });
+        });
+    });
+})();
