2a2f243cde11411dd8513861f4d73cfd3b073432
GenyLeong <genesis.leon.carbajal@gmail.com>
2016-07-26 18:49:38 -0500

    creacion de server.js, Puno is ready

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app  = require('express')();
+var http = require('http').Server(app);
+
+app.use(express.static(__dirname + '/static/'));
+
+
+
+app.get('/data', function (req, res) {
+  res.sendFile( __dirname + '/data.jason');
+})
+
+app.get('/', function(req, res) {
+  res.sendFile( __dirname + '/static/index.html');
+
+});
+
+http.listen(1450, function() {
+  console.log('listening on *:1450');
+});
--- static/js/app.js
+++ static/js/app.js
@@ -3,14 +3,33 @@
         $(document).ready(function () {
             map = new Datamap({
                 element: document.getElementById('map-canvas'),
+                scope: 'per',
                 responsive: false,
                 geographyConfig: {
-                    dataUrl: './static/data/pe-all.topo.json'
-                },
-                scope: 'per',
+                    dataUrl: './data/pe-all.topo.json',
+                    highlightBorderColor: '#000000',
+                    popupTemplate: function(geography, data) {
+                        return '<div class="hoverinfo">' + geography.properties.name + 'Electoral Votes:' +  data.electoralVotes + ' '
+                        },
+                         highlightBorderWidth: 1
+                    },
                 fills: {
-                    defaultFill: '#bada55',
+                        defaultFill: '#bada55',
+                        'Republican': '#CC4731',
+                      'Democrat': '#306596',
+                      'Heavy Democrat': '#667FAF',
+                      'Light Democrat': '#A9C0DE'
                 },
+                data:{
+                          "PE.LB": {
+                              "fillKey": "Republican",
+                              "electoralVotes": 5
+                          },
+                          "PE.TU": {
+                              "fillKey": "Light Democrat",
+                              "electoralVotes": 5
+                          }
+                        },
                 // data: {
                 //     'AFG-1758': {fillKey: 'someKey'},
                 //     'AFG-1747': {fillKey: 'someKey'}
@@ -29,7 +48,7 @@
             });
 
             map.legend();
-
+            map.labels();
             window.addEventListener('resize', function() { map.resize(); });
         });
     });
