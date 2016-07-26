79eb61986b2b70bdeabe9a1585bd1d22cfd3adcb
ZodiacFireworks <martin.vuelta@gmail.com>
2016-07-26 02:40:41 -0500

    Femicide Map - Perú
    Change log:
      * Fast testing with  Afghan map
      * Added nginx configuration file for local testing
    
    Modified files:
      * new file:   nginx.conf
      * modified:   static/data/pe-all.topo.json
      * modified:   static/js/app.js

--- static/js/app.js
+++ static/js/app.js
@@ -5,26 +5,25 @@
                 element: document.getElementById('map-canvas'),
                 responsive: true,
                 geographyConfig: {
-                  dataUrl: './static/data/pe-all.topo.json'
+                    dataUrl: './static/data/af-all.topo.json'
                 },
-                scope: 'peru',
-                projection: 'mercator'
-                // fills: {
-                //   defaultFill: '#bada55',
-                //   someKey: '#fa0fa0'
-                // },
-                // data: {
-                //   'AFG-1758': {fillKey: 'someKey'},
-                //   'AFG-1747': {fillKey: 'someKey'}
-                // },
-                // setProjection: function(element) {
-                //   var projection = d3.geo.mercator()
-                //     .center([66.166667, 34.4444])
-                //     .scale(2200)
-                //     .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
-                //    var path = d3.geo.path().projection(projection);
-                //    return {path: path, projection: projection};
-                // }
+                scope: 'afghan',
+                fills: {
+                    defaultFill: '#bada55',
+                    someKey: '#fa0fa0'
+                },
+                data: {
+                    'AFG-1758': {fillKey: 'someKey'},
+                    'AFG-1747': {fillKey: 'someKey'}
+                },
+                setProjection: function(element) {
+                    var projection = d3.geo.mercator()
+                        .center([66.166667, 34.4444])
+                        .scale(2200)
+                        .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+                    var path = d3.geo.path().projection(projection);
+                    return {path: path, projection: projection};
+                }
             });
 
             window.addEventListener('resize', function() { map.resize(); });
