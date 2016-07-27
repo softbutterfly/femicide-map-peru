4b63ff5c34ba94ded73ae414585852f45383a252
ZodiacFireworks <martin.vuelta@gmail.com>
2016-07-27 14:51:27 -0500

    Femicide Map - Perú
    Change log:
      * Processing and corrections of geo json complete.
      * Prettify of server.js
      * Update .gitignore
    
    Modified files:
      * modified:   .gitignore
      * new file:   processing/Completing and correcting Peru TopoJson data from Peru GeoJson data.ipynb
      * new file:   processing/peru_all_geo.json
      * new file:   processing/peru_all_topo.json
      * new file:   processing/peru_geo.json
      * new file:   processing/peru_topo.json
      * modified:   server.js

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 var express = require('express');
-var app  = require('express')();
+var app = express();
 var http = require('http').Server(app);
 
-app.use(express.static(__dirname + '/static/'));
+app.use(
+    express.static(__dirname + '/static/')
+);
 
-
-
-app.get('/data', function (req, res) {
-  res.sendFile( __dirname + '/data.jason');
+app.get('/data', function(req, res) {
+    res.sendFile(__dirname + '/data.jason');
 })
 
 app.get('/', function(req, res) {
-  res.sendFile( __dirname + '/static/index.html');
-
+    res.sendFile(__dirname + '/static/index.html');
 });
 
 http.listen(1450, function() {
-  console.log('listening on *:1450');
+    console.log('listening on *:1450');
 });
