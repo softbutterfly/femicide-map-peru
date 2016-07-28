b6e64cceafea6133694b955809e579c5e3dbb291
ZodiacFireworks <martin.vuelta@gmail.com>
2016-07-27 19:10:17 -0500

    Femicide Map - Perú
    Change log
      * Purge data files
      * Formated femicide stats in csv and json
    
    Modified files:
      * deleted:    data.csv
      * modified:   index.html
      * modified:   processing/Completing and correcting Peru TopoJson data from Peru GeoJson data.ipynb
      * modified:   processing/peru_geo.json
      * modified:   processing/peru_topo.json
      * modified:   static/css/app.css
      * deleted:    static/data/af-all.geo.json
      * deleted:    static/data/af-all.topo.json
      * new file:   static/data/femicide_stats.csv
      * new file:   static/data/femicide_stats.json
      * deleted:    static/data/pe-all.geo.json
      * deleted:    static/data/pe-all.topo.json
      * new file:   static/data/peru_topo.json
      * new file:   static/img/bg.jpg
      * modified:   static/js/app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,66 +1,82 @@
-(function () {
-    $(window).load(function () {
-        $(document).ready(function () {
-            map = new Datamap({
+(function() {
+    function ceilToNearestHundred(x) {
+        nH = 1;
+        while(x > 10) {
+            x = x/10;
+            nH *= 10;
+        }
+        nH = parseInt(x)*nH
+        console.log(nH,x)
+    }
+
+    $(window).load(function() {
+        $(document).ready(function() {
+            var mapOptions = {
                 element: document.getElementById('map-canvas'),
-                scope: 'per',
                 responsive: false,
                 geographyConfig: {
-                    dataUrl: './data/pe-all.topo.json',
+                    dataUrl: 'static/data/peru_topo.json',
                     highlightBorderColor: '#000000',
                     popupTemplate: function(geography, data) {
-                        return ['<div class="hoverinfo"><b>' + geography.properties.name + '</b><br/>' + 'Electoral Votes:' +  data.electoralVotes + '</div>'].join('');
-                        },
-                         highlightBorderWidth: 1
+                        return [
+                            '<div class="hoverinfo">',
+                            '<b>' + geography.properties.name + '</b>',
+                            '<br/>',
+                            'Electoral Votes:' + data.electoralVotes + '</div>'
+                        ].join('');
                     },
-                fills: {
-                        defaultFill: '#bada55',
-                        'Republican': '#CC4731',
-                          'Democrat': '#306596',
-                          'Heavy Democrat': '#667FAF',
-                          'Light Democrat': '#A9C0DE'
+                    highlightBorderWidth: 1
                 },
-                data:{
-                          "PE.LB": {
-                              "fillKey": "Republican",
-                              "electoralVotes": 5
-                          },
-                          "PE.TU": {
-                              "fillKey": "Light Democrat",
-                              "electoralVotes": 5
-                          },
-                          "PE.AP": {
-                              "fillKey": "Light Democrat",
-                              "electoralVotes": 5
-                          },
-                          "PE.AR": {
-                              "fillKey": "Light Democrat",
-                              "electoralVotes": 5
-                          },
-                          "PE.CS": {
-                              "fillKey": "Light Democrat",
-                              "electoralVotes": 5
-                          }
-                        },
-                // data: {
-                //     'AFG-1758': {fillKey: 'someKey'},
-                //     'AFG-1747': {fillKey: 'someKey'}
-                // },
+                scope: 'peru',
                 setProjection: function(element) {
                     var projection = d3.geo.mercator()
-                        .center([-76.1410154, -11.0674535]) // lng, lat
-                        .scale(1500)
+                        .center([-74.9919444444, -9.18875]) // lng, lat
+                        .scale(4 * element.offsetWidth)
                         .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
 
                     var path = d3.geo.path()
                         .projection(projection);
 
-                    return {path: path, projection: projection};
+                    return { path: path, projection: projection };
                 }
-            });
+            }
 
-            //map.legend();
-            map.labels();
+            var mapData;
+            var mapFillings = {
+                defaultFill: '#bada55',
+            };
+
+            var statsURL = "/static/data/femicide_stats.json"
+            var jqxhr = $.getJSON( statsURL, function(data) {
+                    // Getting data period
+                    $(".period-begin").text(data.period[0]);
+                    $(".period-end").text(data.period[1]);
+                    $(".period").removeClass('hiddendiv');
+
+                    // Gettings mapData
+                    mapData = data.stats;
+
+                    // Hover on Lima Metropolitana will
+                    // show Lima Department stats
+                    mapData["PE.LP"] = mapData["PE.LR"];
+
+                    // Creating Filling Scale
+                    keys = Object.keys(mapData);
+                    femicides = keys.map(function(key){ return mapData[key]["femicides"]; });
+                    tentatives = keys.map(function(key){ return mapData[key]["tentatives"]; });
+
+                    minF = Math.min(femicides);
+                    maxF = Math.max(femicides);
+
+                    minT = Math.min(tentatives);
+                    maxT = Math.max(tentatives);
+
+                    console.log(femicides);
+                    console.log(tentatives);
+                });
+
+            var map = new Datamap(mapOptions);
+
             window.addEventListener('resize', function() { map.resize(); });
         });
     });
